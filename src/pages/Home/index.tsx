fd29ad2c1574b6b1fea43cc4025b42dcc1daebc1
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-06 19:45:12 -0300

    Change title with counter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -65,6 +65,7 @@ export function Home() {
     };
     setCycles((cycles) => [...cycles, newCycle]);
     setActiveCycleId(newCycle.id);
+    setAmoutSecondsPassed(0);
     reset();
   }
 
@@ -76,6 +77,12 @@ export function Home() {
 
   const minutes = String(minutesAmout).padStart(2, "0");
   const seconds = String(secondsAmmout).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const task = watch("task");
   const isSubmitDisabled = !task;
