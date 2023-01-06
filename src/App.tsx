83216b6d28798ee4f2922f8cb04dee3f8930d4ac
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-06 11:07:43 -0300

    Start project, setup styled components, @typetheme

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="danger" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="warning" />
+      <Button />
+    </>
+  );
+}
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant =
+  | "primary"
+  | "secondary"
+  | "danger"
+  | "success"
+  | "warning";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+  warning: "yellow",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  background-color: ${(props) => props.theme["green-500"]};
+  /* ${(props) =>
+    css`
+      background-color: ${buttonVariants[props.variant]};
+    `} */
+`;
