4ffa58d4e1629e4d421808fa3eea87a0b2c6ce07
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-07 13:11:21 -0300

    Bug fixes

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -25,7 +25,7 @@ export function History() {
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmout} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
