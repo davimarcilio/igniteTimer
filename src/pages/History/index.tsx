fd5a9d3f7f096ef19dd4d9c0b2ebd297960e0b3c
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-07 13:14:08 -0300

    Fix

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -37,7 +37,7 @@ export function History() {
                     {cycle.interruptedDate && (
                       <Status statusColor="red">Interrompido</Status>
                     )}
-                    {!cycle.interruptedDate && !cycle.interruptedDate && (
+                    {!cycle.interruptedDate && !cycle.finishedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
                   </td>
